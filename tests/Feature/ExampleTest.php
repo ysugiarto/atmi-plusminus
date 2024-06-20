7038eea917f2753393ede30ecc28c1b7fb6acebb
ysugiarto <yoh.sugiarto@gmail.com>
2024-06-20 14:28:33 +0700

    Fix test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,13 +4,14 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models;
 
 class ExampleTest extends TestCase
 {
     /**
      * @test
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
 
